ac5b487c8875555a865faf483b5006d1e5c06091
latishs sehgal <latish.sehgal@gmail.com>
2011-02-23 20:26:38 -0600

    Initial commit. Need to play around with SVsExtensionManager more.

---
+++ ExtensionSync/ExtensionSync/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace LatishSehgal.ExtensionSync
+{
+    static class GuidList
+    {
+        public const string guidExtensionSyncPkgString = "887b3e11-719d-4912-94b2-f88168027643";
+        public const string guidExtensionSyncCmdSetString = "7e357f67-d5a0-42bb-a105-a0e772476a5f";
+
+        public static readonly Guid guidExtensionSyncCmdSet = new Guid(guidExtensionSyncCmdSetString);
+    };
+}
---
+++ ExtensionSync/ExtensionSync/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace LatishSehgal.ExtensionSync
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidSyncExtensions =        0x100;
+
+
+    };
+}
